85c743fe5e308d4d418cd6ca1f8d0436fd5526ec
Hrushikesh1822 <rushikeshliman2230@gmail.com>
2025-05-27 22:57:31 +0530

    Add files via upload

---
+++ Assign1.java
@@ -0,0 +1,13 @@
+public class Assign1
+{
+public static void main(String[] args)
+{
+ int p = 20000;
+ int r = 6;
+ int t = 4;
+ 
+ double si = p*r*t/100;
+ 
+ System.out.println("The Simple Interest is : "+si);
+}
+}
